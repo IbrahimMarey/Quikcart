431a52e8325f993f86115056ea3de82e42c55819
IbrahimMarey <61487676+IbrahimMarey@users.noreply.github.com>
2024-05-31 17:54:43 +0300

    test price

--- app/src/main/java/com/example/quikcart/utils/Extentions.kt
+++ app/src/main/java/com/example/quikcart/utils/Extentions.kt
@@ -1,7 +1,16 @@
 package com.example.quikcart.utils
 
+import android.content.Context
 import android.widget.TextView
 
-fun TextView.setPrice(price:Float){
-
+fun TextView.setPrice(price:Float,ctx: Context){
+    var txtPrice = price
+    if (!PreferencesUtils.getInstance(ctx).getCurrencyType().equals(PreferencesUtils.CURRENCY_EGP))
+    {
+        txtPrice *= 0.021f
+    }
+    text = buildString {
+        append(txtPrice)
+        append(" ${PreferencesUtils.getInstance(ctx).getCurrencyType()}")
+    }
 }
