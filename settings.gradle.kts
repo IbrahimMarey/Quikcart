702f1dfada5da72ac84c1b9f4f67cfb06b29fcba
IbrahimMarey <61487676+IbrahimMarey@users.noreply.github.com>
2024-06-02 18:31:02 +0300

    paypal payment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,12 +19,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven ( "https://jitpack.io")
-        maven("https://cardinalcommerceprod.jfrog.io/artifactory/android") {
-            credentials {
-                username = "paypal_sgerritz"
-                password = "AKCp8jQ8tAahqpT5JjZ4FRP2mW7GMoFZ674kGqHmupTesKeAY2G8NcmPKLuTxTGkKjDLRzDUQ"
-            }
-        }
+
     }
 }
 
