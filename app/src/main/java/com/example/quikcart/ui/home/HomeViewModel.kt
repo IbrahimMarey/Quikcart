3d676512e274ba4aef18febb707302cbe6b6110c
Nareman <naremansharkan5@gmail.com>
2024-06-19 23:41:21 +0300

    fix coupon slider image

--- app/src/main/java/com/example/quikcart/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/quikcart/ui/home/HomeViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.quikcart.ui.home
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.denzcoskun.imageslider.models.SlideModel
@@ -22,7 +24,10 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(private val repo: Repository) : ViewModel() {
     private val _uiState = MutableStateFlow<ViewState<List<SmartCollectionsItem>>>(ViewState.Loading)
     var uiState: StateFlow<ViewState<List<SmartCollectionsItem>>> = _uiState
-    val couponsList = ArrayList<SlideModel>()
+    /*val couponsList = ArrayList<SlideModel>()
+    val couponsIDs = ArrayList<String>()*/
+    private val _couponsList = MutableLiveData<List<SlideModel>>()
+    val couponsList: LiveData<List<SlideModel>> get() = _couponsList
     val couponsIDs = ArrayList<String>()
 
     init {
@@ -44,8 +49,58 @@ class HomeViewModel @Inject constructor(private val repo: Repository) : ViewMode
            }
         }
     }
+    fun getCoupon() {
+        delCoupons()
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.getCoupons().collect { coupons ->
+                val newCouponsList = mutableListOf<SlideModel>()
+                couponsIDs.clear()
+                getCouponImages(coupons.priceRules, newCouponsList)
+                saveCouponsLocally(coupons.priceRules)
+                _couponsList.postValue(newCouponsList)
+            }
+        }
+    }
 
-    private fun getCouponImages(coupons:List<PriceRule>)
+    private fun delCoupons() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.deleteAllCoupons()
+        }
+    }
+
+    private fun saveCouponsLocally(coupons: List<PriceRule>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.insertAllCoupons(coupons)
+        }
+    }
+
+    private fun getCouponImages(coupons: List<PriceRule>, newCouponsList: MutableList<SlideModel>) {
+        for (item in coupons) {
+            couponsIDs.add(item.id.toString())
+            if (item.valueType == "percentage") {
+                newCouponsList.add(addImgPercentage(item))
+            } else {
+                newCouponsList.add(addImgFixedAmount(item))
+            }
+        }
+    }
+
+    private fun addImgPercentage(coupon: PriceRule): SlideModel {
+        val img = when (coupon.value) {
+            "-20.0" -> "https://t3.ftcdn.net/jpg/03/36/91/14/360_F_336911489_vQzdGPLdY0aNYXdu5rK7UIwwiEksYJgK.jpg"
+            "-30.0" -> "https://static.vecteezy.com/system/resources/previews/004/141/832/non_2x/golden-30-percent-off-flat-cartoon-style-logo-concept-30-percent-sale-isolated-icon-on-black-background-thirty-percent-discount-for-business-illustration-vector.jpg"
+            "-50.0" -> "https://static.vecteezy.com/system/resources/thumbnails/004/141/666/small_2x/golden-50-percent-off-flat-cartoon-style-logo-concept-50-percent-sale-isolated-icon-on-black-background-fifty-percent-discount-for-business-illustration-vector.jpg"
+            else -> "https://cdn.shopify.com/s/files/1/0817/7988/4088/articles/4XOfcVjU6L9Z0yxkgW0WeI_9a7fdb9d-4173-4023-816b-8918cc91229f.jpg?v=1712946016"
+        }
+        return SlideModel(img, "Click To Apply ${coupon.value} %")
+    }
+
+    private fun addImgFixedAmount(coupon: PriceRule): SlideModel {
+        val img = "https://cdn.shopify.com/s/files/1/0817/7988/4088/articles/4XOfcVjU6L9Z0yxkgW0WeI_9a7fdb9d-4173-4023-816b-8918cc91229f.jpg?v=1712946016"
+        return SlideModel(img, "Click To Apply ${coupon.value}")
+    }
+
+   /* private fun getCouponImages(coupons:List<PriceRule>)
     {
         for (item in coupons)
         {
@@ -60,7 +115,7 @@ class HomeViewModel @Inject constructor(private val repo: Repository) : ViewMode
         }
     }
 
-    private fun getCoupon()
+    *//*private fun getCoupon()
     {
         delCoupons()
         couponsList.clear()
@@ -71,7 +126,23 @@ class HomeViewModel @Inject constructor(private val repo: Repository) : ViewMode
                 saveCouponsLocally(it.priceRules)
             }
         }
+    }*//*
+
+    private fun getCoupon() {
+        delCoupons()
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.getCoupons().collect { coupons ->
+                viewModelScope.launch(Dispatchers.Main) {
+                    couponsList.clear()
+                    couponsIDs.clear()
+                    getCouponImages(coupons.priceRules)
+                    saveCouponsLocally(coupons.priceRules)
+                    _couponsList.postValue(couponsList) // Notify observers
+                }
+            }
+        }
     }
+
 
     private fun delCoupons()
     {
@@ -100,6 +171,6 @@ class HomeViewModel @Inject constructor(private val repo: Repository) : ViewMode
     {
         var img = "https://cdn.shopify.com/s/files/1/0817/7988/4088/articles/4XOfcVjU6L9Z0yxkgW0WeI_9a7fdb9d-4173-4023-816b-8918cc91229f.jpg?v=1712946016"
         return SlideModel(img, "Click To Apply ${coupon.value}")
-    }
+    }*/
 
 }
