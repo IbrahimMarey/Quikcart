0affbca0abff608dfd80d59a66261a438dc13fe2
IbrahimMarey <61487676+IbrahimMarey@users.noreply.github.com>
2024-06-23 11:59:02 +0300

    edit orders price

--- app/src/main/java/com/example/quikcart/ui/orderdetails/OrderDetailsFragment.kt
+++ app/src/main/java/com/example/quikcart/ui/orderdetails/OrderDetailsFragment.kt
@@ -1,24 +1,13 @@
 package com.example.quikcart.ui.orderdetails
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.findNavController
-import com.example.quikcart.R
 import com.example.quikcart.databinding.FragmentOrderDetailsBinding
-import com.example.quikcart.models.ViewState
 import com.example.quikcart.models.entities.OrdersItem
-import com.example.quikcart.models.entities.ProductsItem
-import com.example.quikcart.utils.AlertUtil
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class OrderDetailsFragment : Fragment() {
